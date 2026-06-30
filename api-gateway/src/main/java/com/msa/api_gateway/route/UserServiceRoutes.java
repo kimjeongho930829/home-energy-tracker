82339c67d689e6ba69a487071d93642a6ce10d26
kimjeongho930829 <kimjeongho930829@gmail.com>
2026-06-30 17:38:41 +0900

    feat(api-gateway): Gateway 기반 통합 Swagger UI 환경 구축 및 마이크로서비스 API 문서 라우팅 추가

--- api-gateway/src/main/java/com/msa/api_gateway/route/UserServiceRoutes.java
+++ api-gateway/src/main/java/com/msa/api_gateway/route/UserServiceRoutes.java
@@ -1,6 +1,7 @@
 package com.msa.api_gateway.route;
 
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.function.ServerResponse;
 import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -38,4 +40,12 @@ public class UserServiceRoutes {
                 .build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> userServiceApiDocs() {
+        return GatewayRouterFunctions.route("user-service-api-docs")
+                .route(RequestPredicates.path("/docs/user-service/v3/api-docs"), http())
+                .before(uri("http://localhost:8080"))
+                .filter(setPath("/v3/api-docs"))
+                .build();
+    }
 }
